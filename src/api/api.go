b56d907cc9417a94cf04819f634273b441705c05
smalldog124 <panumars@gmail.com>
2018-07-31 16:56:55 +0700

    [Edited]:แก้เทสของฟังก์ชัน GetinfoHandle ให้ทำงานได้

--- src/api/api.go
+++ src/api/api.go
@@ -6,28 +6,25 @@ import (
 	"net/http"
 )
 
-type InfoResponse struct {
-	Grid    [][]string `json:"grid"`
-	PlayerX string     `json:"playerX"`
-	PlayerO string     `json:"playerO"`
-	Turnof  string     `json:"turnof"`
-}
 type Players struct {
 	PlayerX string `json:"playerX"`
 	PlayerO string `json:"playerO"`
 }
+type API struct {
+	Game game.Game
+}
 
-func GetInfoHandle(write http.ResponseWriter, request *http.Request) {
-	infoJson, _ := json.Marshal(GetInfo())
+func (api API) GetInfoHandler(write http.ResponseWriter, request *http.Request) {
+	infoJson, _ := json.Marshal(api.Game.GetInfo())
 	write.Write(infoJson)
 }
 
-func StartGameHandler(write http.ResponseWriter, request *http.Request) {
+func (api *API) StartGameHandler(write http.ResponseWriter, request *http.Request) {
 	var players Players
 	err := json.NewDecoder(request.Body).Decode(&players)
 	if err != nil {
 		http.Error(write, err.Error(), http.StatusInternalServerError)
 	}
-	game.NewGame(players.PlayerX, players.PlayerO)
+	api.Game = game.NewGame(players.PlayerX, players.PlayerO)
 	write.WriteHeader(http.StatusOK)
 }
